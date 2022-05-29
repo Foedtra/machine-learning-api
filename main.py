3d571891c61a2b507b285551e05260e952e7e710
lovelyoyrmia <mokalulovelyo@gmail.com>
2022-05-29 19:50:29 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import base64
+from flask import Flask, request, jsonify
+from model import Model
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+CORS(app, supports_credentials=True)
+
+@app.route('/')
+def home():
+    return 'hello world'    
+
+@app.route('/predict', methods=['POST'])
+def process_image():
+    try:
+        # CREDENTIALS = 'secret_key/test-foedtra-83d59c508da8.json'
+        # base_image = 'https://firebasestorage.googleapis.com/v0/b/project-missing-app.appspot.com/o/image%2Fasdasda.JPG?alt=media&token=56ad8367-7ace-4954-8ff9-2c695e3eedfb'
+        # image_path = '20190420_160631.jpg'
+        request_image = request.json
+        # image = tf.io.decode_image(base64.b64encode(open(image_path, 'rb').read()), channels=3)
+        image = request_image.get('image')
+        # image = base64.b64encode(open(image_path, 'rb').read())
+
+
+        model = Model()
+        prediction = model.predict(image)
+        # convert server response into JSON format.
+        return jsonify({'msg': 'success', 'prediction': prediction})
+
+    except Exception as e:
+        return jsonify({'msg': 'error', 'error': e})
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
---
+++ model.py
@@ -0,0 +1,45 @@
+import base64
+import io
+import json
+import os
+import tensorflow as tf
+from PIL import Image
+from keras_preprocessing import image
+from keras.models import load_model
+
+
+class Model:
+    def __init__(self, model_path=os.path.join(os.getcwd(), 'model', 'foedtra_model3_improve.h5')):
+        self.model_path = model_path
+
+    def _load_model(self):
+        return load_model(self.model_path)
+
+    def _normalizeImage(self, img):
+        img = tf.keras.preprocessing.image.smart_resize(img, (224, 224))
+        img = image.img_to_array(img)
+        img = tf.expand_dims(img, axis=0)
+
+        return img / 255
+
+    def _load_json(self):
+        dataset = json.load(open('dataframe-traditional-food.json', 'r'))
+        for data in dataset['data']:
+            data['keyword'] = data['namaMakanan'].lower().replace(' ', '_')
+    
+        return dataset
+
+
+    def predict(self, img):
+        dataset = self._load_json()
+        model = self._load_model()
+
+        img = Image.open(io.BytesIO(base64.b64decode(img)))
+        img = self._normalizeImage(img)
+        predictions = model.predict(img)
+        predictions = tf.argmax(predictions[0])  
+        prediction = dataset['data'][predictions]
+
+        return prediction
+    
+
